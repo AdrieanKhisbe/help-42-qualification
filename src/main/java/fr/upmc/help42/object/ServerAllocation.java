04f0e09f256c5f69fb6a6f90628a0bbf5def1e45
Adriean Khisbe <adriean.khisbe@live.fr>
2015-03-12 21:34:01 +0100

    solution representation

---
+++ src/main/java/fr/upmc/help42/object/ServerAllocation.java
@@ -0,0 +1,53 @@
+package fr.upmc.help42.object;
+
+/**
+ * @author Adriean Khisbe
+ */
+public class ServerAllocation {
+
+    private Server server;
+    private int group;
+    private int row;
+    private int irow;
+
+
+    public ServerAllocation(Server server, int group, int row, int irow) {
+        this.server = server;
+        this.group = group;
+        this.row = row;
+        this.irow = irow;
+    }
+
+
+    public Server getServer() {
+        return server;
+    }
+
+    public void setServer(Server server) {
+        this.server = server;
+    }
+
+    public int getGroup() {
+        return group;
+    }
+
+    public void setGroup(int group) {
+        this.group = group;
+    }
+
+    public int getRow() {
+        return row;
+    }
+
+    public void setRow(int row) {
+        this.row = row;
+    }
+
+    public int getIrow() {
+        return irow;
+    }
+
+    public void setIrow(int irow) {
+        this.irow = irow;
+    }
+}
--- src/main/java/fr/upmc/help42/problem/DatacenterAllocation.java
+++ src/main/java/fr/upmc/help42/problem/DatacenterAllocation.java
@@ -1,14 +1,25 @@
 package fr.upmc.help42.problem;
 
+import fr.upmc.help42.object.ServerAllocation;
+
+import java.util.List;
+
 /**
  * @author Adriean Khisbe
  */
 public class DatacenterAllocation {
+
+    List<ServerAllocation> servers;
+    
     
     //FIXME: eval method
     private int score;
 
     public int getScore() {
         return score;
+    }
+
+    public List<ServerAllocation> getServers() {
+        return servers;
     }
 }
